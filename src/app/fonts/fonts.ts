22abe8a942be2be992f07d372be3c560f14dbc8a
Bl Bltich <vissar2016@yandex.ru>
2024-03-25 23:43:39 +0300

    feat: added css global fonts

---
+++ src/app/fonts/fonts.ts
@@ -0,0 +1,17 @@
+import { Roboto, Montserrat, Rubik } from "next/font/google";
+
+export const roboto = Roboto({
+  subsets: ["cyrillic", "latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+  display: "block",
+  fallback: ["sans-serif"]
+});
+
+export const montserrat = Montserrat({
+  subsets: ["latin", "cyrillic"],
+  weight: ["400", "700"],
+  variable: "--font-montserrat",
+  display: "block",
+  fallback: ["sans-serif"]
+});
