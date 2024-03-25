c4d22c0b90cf677a1a98f87f45a61cdb8bebd837
Bl Bltich <vissar2016@yandex.ru>
2024-03-25 23:44:13 +0300

    feat: fonts imported

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../src/app/styles/normalize.css';
 import '../src/app/styles/global.css';
+import { montserrat, roboto } from '@/app/fonts/fonts';
 
 const RootLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <html lang="ru" className={``}>
+    <html lang="ru" className={`${montserrat.variable} ${roboto.variable}`}>
       <body>{children}</body>
     </html>
   );
