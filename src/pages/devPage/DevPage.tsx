f70ec02441ba956dddc7ec99c93efeae16d10c46
Bl Bltich <vissar2016@yandex.ru>
2024-03-26 22:07:17 +0300

    fix: path of export component fixed

--- src/pages/devPage/DevPage.tsx
+++ src/pages/devPage/DevPage.tsx
@@ -1,12 +1,16 @@
-'use client';
-
-import React from 'react';
+import React from "react";
+import { Button } from "@/shared/ui/Button";
 export const DevPage: React.FC = () => {
   return (
     <div>
       <h1>Test page</h1>
       <div>
-        <p>Hello World</p>
+        <Button
+          disabled={false}
+          variant="pink"
+        >
+          Читать далее
+        </Button>
       </div>
     </div>
   );
