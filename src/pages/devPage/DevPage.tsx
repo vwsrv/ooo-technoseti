2951985c94d5f33f3b8f589d0480c65c7ce50e93
Bl Bltich <vissar2016@yandex.ru>
2024-04-05 14:28:00 +0300

    feat: added file structure of component

--- src/pages/devPage/DevPage.tsx
+++ src/pages/devPage/DevPage.tsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { Button } from "@/shared/ui/Button";
+import { SectionHeading } from "@/shared/ui/SectionHeading/SectionHeading";
+import { TechCard } from "@/shared/ui/TechCard/TechCard";
+import techImage from "../../shared/images/for-tech-card/tech-one.svg";
+
 export const DevPage: React.FC = () => {
   return (
     <div>
-      <h1>Test page</h1>
       <div>
-        <Button
-          disabled={false}
-          variant="pink"
-        >
-          Читать далее
-        </Button>
+        <TechCard
+          name="Lorem ipsum dolor sit amet."
+          description="Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet"
+          imageUrl={techImage}
+        />
+
+
       </div>
+      <TechCard
+          name="Lorem ipsum dolor sit amet."
+          description="Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet"
+          imageUrl={techImage}
+        />
     </div>
   );
 };
--- src/shared/ui/TechCard/types.ts
+++
@@ -1,9 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes } from "react";
-import { StaticImageData } from "next/image";
-
-export interface TypeTechCardProps
-  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-  name: string;
-  description: string;
-  imageUrl: StaticImageData;
-}
