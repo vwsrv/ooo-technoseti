6660423f327a9903f76699c7d989a4cb3e2d2f42
Bl Bltich <vissar2016@yandex.ru>
2024-10-23 03:03:21 +0300

    feat: added CompanyLogo component

---
+++ src/shared/ui/CompanyLogo/CompanyLogo.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+import cn from "classnames";
+import classes from "./index.module.scss";
+import Image from "next/image";
+import ImageLink from "../../images/main-logo/logo-width-white.svg";
+
+export const CompanyLogo: React.FC = () => {
+  return (
+    <div className={cn(classes.logoContainer)}>
+      <Image
+        src={ImageLink}
+        className={classes.image}
+        alt="Логотип организации"
+      />
+      <div className={classes.logoLine}></div>
+    </div>
+  );
+};
