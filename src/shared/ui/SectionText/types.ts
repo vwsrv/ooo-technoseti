d7ec450d5b8e2295347eb060188817f62e72bc78
Bl Bltich <vissar2016@yandex.ru>
2024-10-23 03:05:11 +0300

    refactor: SectionText component refactored

---
+++ src/shared/ui/SectionText/types.ts
@@ -0,0 +1,9 @@
+import { HTMLAttributes, DetailedHTMLProps } from "react";
+
+export interface TypeSectionTextProps
+  extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
+  title: string;
+  titleBold: string;
+  subtitle?: string;
+  variant?: 'blue' | 'pink' | 'black';
+}
